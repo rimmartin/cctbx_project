9312ec3c380a2469cdfc01d661c7249ccf731963
roger <roger@frodo.quantumbioinc.com>
2019-04-24 09:44:00 -0400

    trying prev ossl

--- libtbx/auto_build/package_defs.py
+++ libtbx/auto_build/package_defs.py
@@ -49,7 +49,7 @@ def get_pypi_package_information(package, version=None, information_only=False):
   return package
 
 DEPENDENCIES_BASE = "https://gitcdn.link/repo/dials/dependencies/master/"
-OPENSSL_PKG = "openssl-1.0.2r.tar.gz"    # OpenSSL
+OPENSSL_PKG = "openssl-1.0.2q.tar.gz"    # OpenSSL
 PYTHON3_PKG = "Python-3.7.2.tgz"
 PYTHON_PKG = "Python-2.7.16.tgz"
 
